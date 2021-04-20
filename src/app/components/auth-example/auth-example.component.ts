bc4d88777d402592ddab95a14512b01ca092ddcc
omarrb76 <omarshark23@gmail.com>
2021-04-20 12:22:03 -0500

    Valide que pongan el numero correcto

--- src/app/components/auth-example/auth-example.component.ts
+++ src/app/components/auth-example/auth-example.component.ts
@@ -1,10 +1,10 @@
 /* Ejemplo para iniciar sesión con número de teléfono */
 
 import { AuthService } from './../../services/firebase/auth.service';
-import { PhoneNumber } from './../../models/PhoneNumber';
-import { FirestoreService } from './../../services/firebase/firestore.service';
 import { WindowService } from './../../services/window.service';
+import { FirestoreService } from './../../services/firebase/firestore.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-example',
@@ -13,58 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthExampleComponent implements OnInit {
 
-  public cats: any;
-
   windowRef: any;                   // Referencia de la ventana (necesaria para que funcione el reCapctha)
-  phoneNumber = new PhoneNumber();  // Número de teléfono (propenso a ser borrado)
   verificationCode: string = "";    // Código de verificación
   user: any;                        // El usuario que inicio sesión
+  phoneNumber: FormGroup = null!;   // Formulario de número de teléfono
+  error: string = "";               // Mostrar mensaje de error
 
   constructor(
-    private firestoreService: FirestoreService,
+    private firestoreService: FirestoreService,     // No se porque, pero no funciona si le quitan este servicio
     private windowService: WindowService,
     private authService: AuthService
-  ) { }
+  ) {
+    this.phoneNumber = new FormGroup({
+      number: new FormControl(null, [Validators.required, Validators.pattern('[0-9]{10}')]),
+    });
+  }
 
   ngOnInit() {
-
-    /* this.firestoreService.getCats().subscribe((catsSnapshot) => {
-      this.cats = [];
-      catsSnapshot.forEach((catData: any) => {
-        this.cats.push({
-          id: catData.payload.doc.id,
-          data: catData.payload.doc.data()
-        });
-      })
-    }); */
-
     // Obtenemos la referencia de la ventana y dibujamos el reCaptcha
     this.windowRef = this.windowService.getWindowRef();
     this.windowRef.recaptchaVerifier = this.authService.recaptchaVerifier();
     this.windowRef.recaptchaVerifier.render();
-
   }
 
   sendLoginCode() {
 
     const appVerifier = this.windowRef.recaptchaVerifier;
-    const num = this.phoneNumber.e164;
+    const num = '+52' + this.phoneNumber.value.number;
 
     this.authService.signInWithPhoneNumber(num, appVerifier)
-    .then(result => {
-      this.windowRef.confirmationResult = result;
-    })
-    .catch(error => console.log(error));
+      .then(result => {
+        this.windowRef.confirmationResult = result;
+      })
+      .catch(error => console.log(error));
 
   }
 
   verifyLoginCode() {
     this.windowRef.confirmationResult
-    .confirm(this.verificationCode)
-    .then((result: any) => {
-      this.user = result.user;
-    })
-    .catch((error: any) => console.log(error, 'Incorrect code entered'));
+      .confirm(this.verificationCode)
+      .then((result: any) => {
+        this.user = result.user;
+      })
+      .catch((error: any) => console.log(error, 'Incorrect code entered'));
+  }
+
+  keyUp() {
+    if (this.phoneNumber.valid) {
+      this.error = "";
+    } else {
+      this.error = "Ingresa un número de teléfono válido";
+    }
   }
 
 }
